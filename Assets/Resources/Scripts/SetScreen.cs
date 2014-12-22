fb4b345990fdd6b2d3939126a1dd57c86329711b
Nickchooshin <gustncjswp95@gmail.com>
2014-12-23 08:03:14 +0900

    빌드 셋팅

---
+++ Assets/Resources/Scripts/SetScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetScreen : MonoBehaviour {
+
+    void Start()
+    {
+        Screen.SetResolution(Screen.width, Screen.width/16*9, true);
+    }
+}
