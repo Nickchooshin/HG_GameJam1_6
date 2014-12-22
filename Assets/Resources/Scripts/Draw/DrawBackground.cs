b3353a9f2177d1f392f54126064c8a6c5274da3e
Nickchooshin <gustncjswp95@gmail.com>
2014-12-23 00:29:23 +0900

    Background_Text 추가, 챕터별 목소리 재생 추가

--- Assets/Resources/Scripts/Draw/DrawBackground.cs
+++ Assets/Resources/Scripts/Draw/DrawBackground.cs
@@ -4,17 +4,55 @@ using System.IO;
 
 public class DrawBackground : MonoBehaviour {
 
+    public SpriteRenderer background;
+    public SpriteRenderer background_text;
+
+    private AudioSource audioSource;
+
+    private float m_fWaitTime ;
+
     void Start()
     {
         TextAsset textAsset = Resources.Load("txt/DrawBackground" + StageState.Instance.NowStage()) as TextAsset;
         TextReader reader = new StringReader(textAsset.text);
 
         string strBackground = reader.ReadLine();
+        string strAudio = reader.ReadLine();
+        m_fWaitTime = float.Parse(reader.ReadLine());
 
         reader.Close();
 
-        SpriteRenderer spriteRender = gameObject.GetComponent<SpriteRenderer>();
-        spriteRender.sprite = Resources.Load(strBackground, typeof(Sprite)) as Sprite;
-        spriteRender.material.renderQueue = 0;
+        background.sprite = Resources.Load(strBackground, typeof(Sprite)) as Sprite;
+        background.material.renderQueue = 0;
+
+        background_text.sprite = Resources.Load(strBackground + "_text", typeof(Sprite)) as Sprite;
+        background_text.material.renderQueue = 0;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(Resources.Load(strAudio, typeof(AudioClip)) as AudioClip);
+
+        StartCoroutine("FadeOut");
+    }
+
+    IEnumerator FadeOut()
+    {
+        const float fFadeOutTime = 1.5f;
+        float fTime = 0.0f;
+
+        yield return new WaitForSeconds(m_fWaitTime);
+
+        while (true)
+        {
+            background_text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f - (fTime / fFadeOutTime));
+
+            if (fTime >= fFadeOutTime)
+            {
+                Destroy(background_text);
+                StopCoroutine("FadeOut");
+            }
+
+            yield return null;
+            fTime += Time.deltaTime;
+        }
     }
 }
