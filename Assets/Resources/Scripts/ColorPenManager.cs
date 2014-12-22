44ba44f61d0ec547c0a903f2da915b58c4bedc3d
Nickchooshin <gustncjswp95@gmail.com>
2014-12-22 11:09:24 +0900

    ColorPenManager 추가

---
+++ Assets/Resources/Scripts/ColorPenManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ColorPenManager : MonoBehaviour {
+
+    public List<GameObject> PenList;
+    private GameObject selectedPen;
+    private Vector3 vecPenMove = new Vector3(0.0f, 0.2f, 0.0f);
+
+    void Start()
+    {
+        selectedPen = PenList[0];
+        selectedPen.transform.position += vecPenMove;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                foreach (GameObject Pen in PenList)
+                {
+                    if (hit.collider.gameObject == Pen)
+                    {
+                        selectedPen.transform.position -= vecPenMove;
+                        selectedPen = Pen;
+                        selectedPen.transform.position += vecPenMove;
+
+                        PenColor penColor = Pen.GetComponent<PenColor>();
+                        DrawColor.Instance.color = penColor.color;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/PenColor.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenColor : MonoBehaviour {
+
+    public Color color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+}
