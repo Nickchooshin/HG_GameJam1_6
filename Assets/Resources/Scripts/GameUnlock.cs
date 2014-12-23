af45be53f70d5c68fc7c1110b31850a3794a4418
Nickchooshin <gustncjswp95@gmail.com>
2014-12-23 14:38:25 +0900

    pathForDocumentsFile 추가, GameUnlock.cs 추가

---
+++ Assets/Resources/Scripts/GameUnlock.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class GameUnlock {
+
+    private bool m_bGameUnlock;
+
+    public GameUnlock()
+    {
+        FileStream fs;
+
+        string filepath = pathForDocumentsFile.path("game.dat");
+
+        fs = new FileStream(filepath, FileMode.Open);
+        if (fs == null)
+        {
+            fs = new FileStream(filepath, FileMode.CreateNew);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(false);
+            sw.Close();
+            fs.Close();
+
+            fs = new FileStream(filepath, FileMode.Open);
+        }
+        StreamReader sr = new StreamReader(fs);
+
+        m_bGameUnlock = bool.Parse(sr.ReadLine());
+
+        sr.Close();
+        fs.Close();
+    }
+
+    public bool BeGameUnlock()
+    {
+        return m_bGameUnlock;
+    }
+
+    public void DoGameUnlock()
+    {
+        if (!m_bGameUnlock)
+        {
+            FileStream fs = new FileStream(pathForDocumentsFile.path("game.dat"), FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            m_bGameUnlock = true;
+            sw.WriteLine(m_bGameUnlock);
+
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/pathForDocumentsFile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public static class pathForDocumentsFile {
+
+    public static string path(string filename)
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            string path = Application.dataPath.Substring(0, Application.dataPath.Length - 5);
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(Path.Combine(path, "Documents"), filename);
+        }
+
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            string path = Application.persistentDataPath;
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(path, filename);
+        }
+
+        else
+        {
+            string path = Application.dataPath;
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(path, filename);
+        }
+    }
+}
